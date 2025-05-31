d08226eb74dccceecbf03c7778d4f5a410f1879b
Manfredi Bernardi <manfredi.bernardi3108@gmail.com>
2025-05-31 11:24:23 +0200

    Added location selection

--- airbnb_clone/airbnb_clone-frontend/src/components/modals/RentModal.tsx
+++ airbnb_clone/airbnb_clone-frontend/src/components/modals/RentModal.tsx
@@ -5,6 +5,7 @@ import Heading from "../Heading";
 import { categories } from "../navbar/Categories";
 import CategoryInput from "../inputs/CategoryInput";
 import { FieldValues, useForm } from "react-hook-form";
+import CountrySelect from "../inputs/CountrySelect";
 
 enum STEPS {
     CATEGORY = 0,
@@ -41,6 +42,7 @@ const RentModal = () => {
     });
 
     const category = watch('category');
+    const location = watch('location')
 
     const setCustomValue = (id: string, value: any) => {
         setValue(id, value, {
@@ -101,14 +103,28 @@ const RentModal = () => {
                 ))} 
             </div>
         </div>
-
     )
+
+    if (step === STEPS.LOCATION) {
+        bodyContent = (
+            <div className="flex flex-col gap-8">
+                <Heading 
+                    title="Where is your place located?"
+                    subtitle="Help guests find you!"
+                />
+                    <CountrySelect 
+                        value={location}
+                        onChange={(value) => setCustomValue('location', value)}
+                        />   
+            </div>
+        )
+    }
 
     return (
         <Modal
             isOpen={rentModal.isOpen}
             onClose={rentModal.onClose}
-            onSubmit={rentModal.onClose}
+            onSubmit={onNext}
             title="Airbnb your home"
             actionLabel={actionLabel}
             secondaryActionLabel={secondaryActionLabel}
